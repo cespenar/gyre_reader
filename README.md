7301ae59a92a05a3e89fc1360de37aab16023141
cespenar <cespenar1@gmail.com>
2021-02-26 10:25:35 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# gyre_reader
+=================
+
+Tool for accessing a calculated GYRE models. 
+
+## Installation
+Install by cloning the repository, `cd` into it and then execute
+
+    pip install .
+    
+to  install the package on your system.
+
+## Uninstallation
+Uninstall by executing
+
+    pip uninstall gyre_reader
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gyre_reader',
+    version='0.0.1',
+    description='Tool for accessing a calculated GYRE models.',
+    url='https://github.com/cespenar/gyre_reader',
+    author='Jakub Ostrowski',
+    author_email='cespenar1@gmail.com',
+    license='MIT',
+    packages=['gyre_reader'],
+    install_requires=['numpy'])
